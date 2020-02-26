724d9b9165d8606410753b41d5a13790edfb5af1
Ryosuke Fuse <ryosuke.fuse@fullmarks.co.jp>
2020-02-26 14:27:32 +0900

    add login design

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -6,7 +8,27 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  form!: FormGroup
+  errorMessage!: string | null
 
-  ngOnInit(): void {}
+  constructor(private router: Router, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      userId: [null, []],
+      password: [null, []]
+    })
+    this.errorMessage = null
+  }
+
+  get userId(): FormControl {
+    return this.form.get('userId') as FormControl
+  }
+  get password(): FormControl {
+    return this.form.get('password') as FormControl
+  }
+
+  login() {
+    this.router.navigate([''])
+  }
 }
