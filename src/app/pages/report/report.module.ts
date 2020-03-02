39b6a8604ea00ee722801be46459e27ba6197196
Ryosuke Fuse <ryosuke.fuse@fullmarks.co.jp>
2020-03-02 10:22:56 +0900

    add report search component add report table component

--- src/app/pages/report/report.module.ts
+++ src/app/pages/report/report.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core'
 
 import { ReportDetailComponent } from './report-detail/report-detail.component'
 import { ReportListComponent } from './report-list/report-list.component'
-import { ReportRoutingModule } from './report-routing.module'
+import { ReportRoutingModule } from './report-routing.module';
+import { SearchComponent } from './report-shared/components/search/search.component';
+import { TableComponent } from './report-shared/components/table/table.component'
 
 @NgModule({
-  declarations: [ReportListComponent, ReportDetailComponent],
+  declarations: [ReportListComponent, ReportDetailComponent, SearchComponent, TableComponent],
   imports: [ReportRoutingModule]
 })
 export class ReportModule {}
